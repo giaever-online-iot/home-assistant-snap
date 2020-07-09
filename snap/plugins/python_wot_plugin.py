7818f8e3bfc79207fde8734c8f1640b91be577f2
Joachim M. Giæver <joachim@giaever.online>
2020-07-09 17:55:23 +0200

    Modify plugin

---
+++ snap/plugins/python_wot_plugin.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import snapcraft
+import snapcraft.plugins.v2 as plugins
+from typing import Any, Dict, List, Set
+
+"""
+See snapcraft.plugins.v2.python.PythonPlugin
+
+Plugin forces removal of 'typing' package
+after setup, as 'typing' is causing problems
+for python >= 3.7, see:
+
+- https://github.com/pypa/pip/issues/8272
+"""
+class PluginImpl(plugins.python.PythonPlugin):
+    def get_build_commands(self) -> List[str]:
+        cmds = super().get_build_commands()
+        for idx, cmd in enumerate(cmds):
+            # Find position where to inject pip uninstall
+            if cmd.strip().startswith('[ -f setup.py ]'):
+                xcmds = cmd.split("&&", 1)
+                print("Found ", xcmds)
+                # Inject and force removal
+                cmds[idx] = f"{xcmds[0]} && pip uninstall -y typing && {xcmds[1]}"
+        return cmds
