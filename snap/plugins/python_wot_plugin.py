b8a6e5f17dbae76fc3dbe067ab1b4503518c0f73
Joachim M. Giæver <joachim@giaever.online>
2021-07-09 16:51:17 +0200

    Upgrade pip, add libswresample

--- snap/plugins/python_wot_plugin.py
+++ snap/plugins/python_wot_plugin.py
@@ -17,11 +17,13 @@ for python >= 3.7, see:
 class PluginImpl(plugins.python.PythonPlugin):
     def get_build_commands(self) -> List[str]:
         cmds = super().get_build_commands()
-        # cmds.insert(1, 'pip install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org --upgrade pip')
+        cmds.insert(1, 'python3 -m pip install --upgrade pip');
+        #cmds.insert(1, 'pip install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org --upgrade pip')
         for idx, cmd in enumerate(cmds):
             # Find position where to inject pip uninstall
             if cmd.strip().startswith('[ -f setup.py ]'):
                 xcmds = cmd.split("&&", 1)
                 # Inject and force removal
+                # cmds[idx] = f"{xcmds[0]} && pip uninstall -y typing uuid && {xcmds[1]}"
                 cmds[idx] = f"{xcmds[0]} && pip uninstall -y typing uuid && {xcmds[1]}"
         return cmds
